a3607285ec407689e1089f1d43cb423ea7bd279b
Oshan Mudannayake <oshan.ivantha@gmail.com>
2018-09-27 00:34:14 +0530

    Add component structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,36 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { LibraryComponent } from './library/library.component';
+import { OcrComponent } from './ocr/ocr.component';
+import { GrammarComponent } from './grammar/grammar.component';
+import { ConfusionMatrixComponent } from './confusion-matrix/confusion-matrix.component';
+import { ComparisonComponent } from './comparison/comparison.component';
+import { PreferencesComponent } from './preferences/preferences.component';
+import { ProfileComponent } from './profile/profile.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent,
+    LibraryComponent,
+    OcrComponent,
+    GrammarComponent,
+    ConfusionMatrixComponent,
+    ComparisonComponent,
+    PreferencesComponent,
+    ProfileComponent,
+    SignInComponent,
+    SignUpComponent,
+    DashboardComponent,
   ],
   imports: [
     BrowserModule
---
+++ src/app/confusion-matrix/confusion-matrix.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-confusion-matrix',
+  templateUrl: './confusion-matrix.component.html',
+  styleUrls: ['./confusion-matrix.component.css']
+})
+export class ConfusionMatrixComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
