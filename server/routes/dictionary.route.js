1df3ad6cb6df1d07c3c3cb515ac64b2fb529f209
Sumedhe Dissanayake <sumedhedissanayake@gmail.com>
2018-10-13 14:19:34 +0530

    Create dictionary routes

--- server/routes/dictionary.route.js
+++ server/routes/dictionary.route.js
@@ -2,6 +2,33 @@ const express = require('express');
 
 const router     = express.Router()
 
-// TODO : Dictionary routes
-
 module.exports = router;
+
+// Get all words in the dictionary
+router.get('/get-all-words', (req, res) => {
+  var dictionary = require('../controllers/grammar/dictionary-controller');
+
+  dictionary.getAllWords(function (err, result){
+    res.send(result);
+  })
+});
+
+// Get all words in the dictionary
+router.get('/add-word', (req, res) => {
+  var dictionary = require('../controllers/grammar/dictionary-controller');
+  var word = req.query.word;
+
+  dictionary.addWord(word, function (err, result){
+    res.send(result);
+  })
+});
+
+// Apply mandatory rules
+router.post('/apply-mandatory-rules', (req, res) => {
+  var controller = require('../controllers/grammar/mandatory-rule-controller');
+  var text = req.body.text;
+
+  controller.applyMandatoryRules(text, function (err, result){
+    res.send(result);
+  })
+});
--- server/routes/index.route.js
+++ server/routes/index.route.js
@@ -9,52 +9,9 @@ const router     = express.Router()
 router.use('/common', commonRoutes);
 router.use('/dictionary', dictionaryRoutes);
 
-
-
-
-
-
-
-
-// TODO : Move the routes
 /* GET api listing. */
 router.get('/', (req, res) => {
   res.send('api works')
 })
-
-// Get all words in the dictionary
-router.get('/dictionary/get-all-words', (req, res) => {
-  var dictionary = require('../controllers/grammar/dictionary-controller');
-
-  dictionary.getAllWords(function (err, result){
-    console.log(result)
-    res.send(result);
-  })
-});
-
-// Get all words in the dictionary
-router.get('/dictionary/add-word', (req, res) => {
-  var dictionary = require('../controllers/grammar/dictionary-controller');
-  var word = req.query.word;
-
-  dictionary.addWord(word, function (err, result){
-    console.log(result)
-    res.send(result);
-  })
-});
-
-// Apply mandatory rules
-router.post('/dictionary/apply-mandatory-rules', (req, res) => {
-  var controller = require('../controllers/grammar/mandatory-rule-controller');
-  var text = req.body.text;
-
-  controller.applyMandatoryRules(text, function (err, result){
-    res.send(result);
-  })
-});
-
-
-
-
 
 module.exports = router
