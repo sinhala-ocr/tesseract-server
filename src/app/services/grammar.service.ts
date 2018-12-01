bd46fa129665dbd8f4182d607ef7f5934c37316f
Sumedhe Dissanayake <sumedhedissanayake@gmail.com>
2018-12-02 00:50:04 +0530

    Modify grammar service

--- src/app/services/grammar.service.ts
+++ src/app/services/grammar.service.ts
@@ -9,24 +9,21 @@ export class GrammarService {
   constructor(private http: HttpClient) {
   }
 
-  readonly baseURL = 'http://localhost:4000/api/grammar';
+  readonly baseURL = 'http://localhost:8080/grammar';
 
   // Grammar check for output
-  grammarCheck(filename: string) {
-    let data = {
-      filename: filename
-    };
-    return this.http.post(this.baseURL, data);
+  grammarCheck(path: string) {
+    return this.http.get(this.baseURL + '/process?testDirPath=' + path);
   }
 
   // Get File list
   getOutputFileList() {
-    return this.http.get(this.baseURL + '/output-file-list');
+    return this.http.get(this.baseURL + '/get-test-dir-list');
   }
 
   // Load file from the server
   loadFile(path: string) {
-    return this.http.get(this.baseURL + '/load-file?filename=' + path);
+    return this.http.get(this.baseURL + '/load-recog-output?testDirPath=' + path);
   }
 
 }
